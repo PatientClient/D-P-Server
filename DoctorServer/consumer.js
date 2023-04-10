40cfe5f0291a74aa6d88d1853d2f5d705df1b4b2
Yous3f <yasilah4@gmail.com>
2023-04-11 02:54:30 +0300

    added another queue to produce Notifaction to the Doctor Dashboard

--- DoctorServer/consumer.js
+++ DoctorServer/consumer.js
@@ -7,24 +7,49 @@ async function consumeMessages() {
     const channel = await connection.createChannel();
 
     const exchangeName = config.rabbitMQ.exchangeName;
+
+
+
+
+
+    const queueAlert = 'AlertQueue';
+    await channel.assertExchange(exchangeName, 'direct');
+    const qAlert = await channel.assertQueue(queueAlert);
+    await channel.bindQueue(qAlert.queue, exchangeName, 'UL');
+
     const queueName = 'userProgress';
     await channel.assertExchange(exchangeName, 'direct');
 
     const q = await channel.assertQueue(queueName);
     await channel.bindQueue(q.queue, exchangeName, 'UP');
 
-    channel.consume(
-      q.queue,
-      (msg) => {
-        if (msg.content) {
-          const data = JSON.parse(msg.content.toString());
-          console.log('Received message:', data);
-        }
-        channel.ack(msg);
-      }, {
-        noAck: false
-      }
-    );
+    // channel.consume(
+    //   q.queue,
+    //   (msg) => {
+    //     if (msg.content) {
+    //       const data = JSON.parse(msg.content.toString());
+    //       console.log('Received message:', data);
+    //     }
+    //     // channel.ack(msg);
+    //   }
+    //   // , {
+    //   //   noAck: false
+    //   // }
+    // );
+
+    // channel.consume(
+    //   qAlert.queue,
+    //   (msg) => {
+    //     if (msg.content) {
+    //       const data = JSON.parse(msg.content.toString());
+    //       console.log('Received message qalert:', data);
+    //     }
+    //     // channel.ack(msg);
+    //   }
+    //   // , {
+    //   //   noAck: false
+    //   // }
+    // );
   } catch (error) {
     console.error('Error:', error);
   }
